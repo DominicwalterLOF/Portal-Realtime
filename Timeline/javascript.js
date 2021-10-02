8841e42aa3df3425ef8031117f6880a21d120146
deepti <75380267+deepti-3101@users.noreply.github.com>
2021-10-02 22:01:00 +0530

    Update javascript.js

--- Timeline/javascript.js
+++ Timeline/javascript.js
@@ -25,6 +25,61 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+function openForm2() {
+  document.getElementById("myForm2").style.display = "block";
+}
+function closeForm2() {
+  document.getElementById("myForm2").style.display = "none";
+}
   
 
 //-----------------------------------------------------------
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNsNa2W1Xpp1arD_KQldqEpOPCP51pkDU",
+  authDomain: "portal-realtime.firebaseapp.com",
+  databaseURL: "https://portal-realtime-default-rtdb.firebaseio.com",
+  projectId: "portal-realtime",
+  storageBucket: "portal-realtime.appspot.com",
+  messagingSenderId: "385255785015",
+  appId: "1:385255785015:web:6ee27cb5970f49af05b5b2",
+  measurementId: "G-NFTMTJZND7"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function writedata (){
+
+  var mname=document.getElementById("mname").value;
+  var aname=document.getElementById("aname").value;
+  var log=document.getElementById("log").value;
+  var textlog=document.getElementById("textlog").value;
+
+  const dp=firebase.database().ref("FC").set({
+    Mission_name:mname,
+    author:aname,
+    Tagline: log,
+    Log_info:textlog
+
+  });
+}
+
+
+function writedata2 (){
+  var keyvalue=0;
+  var mname=document.getElementById("mname1").value;
+  var aname=document.getElementById("aname1").value;
+  var log=document.getElementById("log1").value;
+  var textlog=document.getElementById("textlog1").value;
+
+  const dp2=firebase.database().ref("BSC").set({
+    Mission_name:mname,
+    author:aname,
+    Tagline: log,
+    Log_info:textlog
+
+  });
+}
+
