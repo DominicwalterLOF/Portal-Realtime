53723b27b23949176202aaf509108df99bf30874
Dominic Walter <59534138+DominicwalterLOF@users.noreply.github.com>
2021-10-03 08:49:33 +0530

    Created Multi User network index

---
+++ network/demo.js
@@ -0,0 +1,19 @@
+function addnode(info){
+
+    document.getElementById("timelinemain").innerHTML = info + document.getElementById("timelinemain").innerHTML;
+
+}
+
+
+function space(){
+    in1 = '<li><div class="direction-r"><div class="flag-wrapper"><span class="flag"> <img src="https://freedesignfile.com/upload/2017/08/spaceship-icon-vector.png"alt="profile image"> Pilot 3</span><span class="time-wrapper"><span class="time">3:55 p.m.</span></span></div><div class="desc"><textarea style = "background: none; border:none; overflow:auto; height: 100px; width:300px;">This is a message received from Spacecraft</textarea></div></div></li>';
+    addnode(in1);
+}
+
+function earth(){
+    in1 = '<li><div class="direction-l"><div class="flag-wrapper"><span class="flag"> <img src="https://icon-library.com/images/icon-earth/icon-earth-14.jpg"alt="profile image">Chief Command</span><span class="time-wrapper"><span class="time">3:55 p.m.</span></span></div><div class="desc">This is a message received from Base Command Center </div></div></li>';
+    addnode(in1);
+
+
+}
+
